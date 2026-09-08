4182c633952aeedcb1a73ed07f3b8f1cf2d8ef50
08es130-max <08es130@gmail.com>
2026-09-09 03:25:33 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.LOVEPOKE_FIREBASE_CONFIG = {
-  apiKey: "ここにFirebaseのapiKey",
+  apiKey: "AIzaSyAUy2Dn-KRkt_LH8NkbgyzbtMuho7vECLM",
   authDomain: "lovepoke-tools.firebaseapp.com",
   projectId: "lovepoke-tools",
-  storageBucket: "ここにFirebaseのstorageBucket",
-  messagingSenderId: "ここにFirebaseのmessagingSenderId",
-  appId: "ここにFirebaseのappId"
+  storageBucket: "lovepoke-tools.firebasestorage.app",
+  messagingSenderId: "450305637358",
+  appId: "1:450305637358:web:2ef37852b9803dba116cff"
 };
