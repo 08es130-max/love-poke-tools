4e12f5b1e1214b6d3fefe29b18809bef6b2f3b22
08es130-max <08es130@gmail.com>
2026-09-09 03:16:18 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,8 @@
-// Firebase Consoleで発行されたWebアプリ設定に置き換えてください。
-// この設定値はFirebaseクライアントを接続するための公開識別情報です。
-window.LOVEPOKE_FIREBASE_CONFIG = null;
+window.LOVEPOKE_FIREBASE_CONFIG = {
+  apiKey: "ここにFirebaseのapiKey",
+  authDomain: "lovepoke-tools.firebaseapp.com",
+  projectId: "lovepoke-tools",
+  storageBucket: "ここにFirebaseのstorageBucket",
+  messagingSenderId: "ここにFirebaseのmessagingSenderId",
+  appId: "ここにFirebaseのappId"
+};
